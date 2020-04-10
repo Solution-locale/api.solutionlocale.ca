6fd87f90df5f5b6dc80e5029f51ec978fb43cab6
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2020-04-09 20:15:05 -0400

    Contextual alert for infos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,7 +20,16 @@
                         <li>Vous êtes approuvés pour une utilisation en écriture l'API: <b>{{ $user->can_write ? "Oui" : "non" }}</b>.</li>
                         <li><em>Rate limit</em> actuelle : {{ $user->rate_limit }} appels d'API par minutes.</li>
                     </ul>
+                    <div class="alert alert-warning">
+                        <p>Pour obtenir une augmentation du nombre d'API que vous pouvez faire, merci de contacter</p>
+                        <p>Jean-Philippe&nbsp;Murray à l'adresse <a href="mailto:jp@atomescroch.us" class="text-reset">jp@atomescroch.us</a></p> 
+                    </div>
                 </div>
+            </div>
+
+            <div class="alert alert-info mt-3">
+                <p>La seule contrainte d'utilisation de notre API est d'en faire l'attribution si vous utilisez ses données. Une mention devrait se trouver dans votre site, ou application, qui devrait se lire comme suit: </p>
+                <p><code>Données provenant en tout, ou en partie, de solutionlocale.ca</code>.</p>
             </div>
 
             @can('use-api')
